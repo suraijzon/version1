2deed9d01d312168ce96036f52679832f93e014c
Suraij <ahamsuraij@gmail.com>
2026-03-06 17:23:37 +0530

    cleanup project remove backend

--- server/middleware/admin.js
+++
@@ -1,8 +0,0 @@
-// middleware/admin.js
-module.exports = function(req, res, next) {
-  // Check if user role is admin
-  if (req.user.role !== 'admin') {
-    return res.status(403).json({ message: 'Access denied. Admin only.' });
-  }
-  next();
-};
